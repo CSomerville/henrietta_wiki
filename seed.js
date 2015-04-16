580038d348db2811ecbce80afb4a027df7aaf357
Colby Somerville <colby.somerville@gmail.com>
2015-04-16 11:55:00 -0400

    going back into the db

--- seed.js
+++ seed.js
@@ -1,11 +1,32 @@
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('./wiki.db');
 
-var content1 = "Carles bicycle rights tattooed, Bushwick readymade biodiesel Intelligentsia literally American Apparel distillery street art sriracha seitan. Butcher hashtag McSweeney's drinking vinegar, vinyl skateboard asymmetrical polaroid cornhole artisan disrupt Carles food truck. Asymmetrical tilde Godard cray, beard scenester gentrify YOLO kogi Odd Future single-origin coffee cred. Leggings 90's wayfarers tattooed iPhone. Tousled put a bird on it messenger bag locavore normcore, next level Echo Park pug gluten-free bitters fashion axe salvia. Pitchfork leggings migas chia, organic meh pug listicle try-hard. PBR Intelligentsia cornhole semiotics iPhone, letterpress bitters pickled distillery kale chips crucifix selfies dreamcatcher High Life.".replace(/'/g, "")
+var ipsumJunk = "Tote bag disrupt chambray roof party, street art polaroid flexitarian irony keytar Wes Anderson PBR&B Portland aesthetic authentic. Heirloom trust fund roof party, vegan cold-pressed Banksy 3 wolf moon scenester Carles. Pitchfork plaid deep v ethical occupy umami. Carles typewriter mixtape beard stumptown trust fund. XOXO trust fund single-origin coffee, wayfarers cardigan chillwave YOLO kogi shabby chic umami four loko. Slow-carb quinoa dreamcatcher meggings Etsy synth. Slow-carb before they sold out cray drinking vinegar, stumptown Williamsburg flexitarian. Brunch slow-carb readymade kale chips. 3 wolf moon migas meditation, yr disrupt Pitchfork umami four dollar toast PBR&B. Hoodie lo-fi viral, Shoreditch cliche hashtag Carles migas master cleanse Truffaut leggings before they sold out seitan Tumblr. Pour-over next level pickled, put a bird on it lo-fi Marfa Wes Anderson butcher pop-up fixie flannel crucifix sartorial. Try-hard sustainable vegan 90's. Gentrify bespoke tote bag Helvetica, chia food truck PBR literally you probably haven't heard of them scenester. Gentrify plaid authentic, polaroid cliche cold-pressed locavore Echo Park whatever art party lomo food truck. Helvetica viral photo booth Godard stumptown mlkshk bitters, chambray irony bicycle rights pop-up skateboard keffiyeh roof party. Dreamcatcher Pinterest fingerstache, pug sustainable YOLO Carles squid PBR letterpress. Sriracha chia Williamsburg craft beer. 8-bit crucifix kitsch stumptown, leggings forage salvia Echo Park next level Intelligentsia. Ennui Shoreditch aesthetic, brunch PBR&B retro stumptown Etsy beard Pitchfork squid Odd Future art party vinyl 8-bit. Blue Bottle fap quinoa, dreamcatcher biodiesel deep v American Apparel fingerstache pour-over ennui. Freegan Godard Bushwick photo booth, selfies tousled bitters Austin drinking vinegar. +1 Marfa pour-over disrupt 3 wolf moon roof party, twee food truck fixie semiotics American Apparel banh mi synth Helvetica. Pug hoodie twee bicycle rights. Stumptown tattooed Neutra farm-to-table, sartorial Etsy blog PBR deep v retro dreamcatcher twee Tumblr viral cronut. Blue Bottle narwhal listicle, street art cray four loko disrupt fanny pack deep v letterpress asymmetrical. Banksy scenester YOLO, McSweeney's asymmetrical 3 wolf moon banjo vegan hoodie cold-pressed skateboard small batch tofu. Authentic Vice American Apparel actually chia, PBR&B High Life fashion axe umami distillery. 3 wolf moon direct trade Echo Park, church-key next level locavore swag 8-bit sustainable 90's readymade skateboard.".replace(/'/g, "''")
 
-var content2 = "Schlitz you probably haven't heard of them selfies selvage. Put a bird on it freegan readymade narwhal 3 wolf moon. Kale chips Marfa Pitchfork next level keytar. McSweeney's dreamcatcher VHS church-key. Ethical American Apparel bitters organic Schlitz skateboard. 3 wolf moon mixtape normcore hashtag dreamcatcher swag. 90's four loko iPhone migas butcher blog, beard quinoa hella bespoke.".replace(/'/g, "")
+function randStart(string) {
+  var array = string.split(".")
+  var rando = Math.floor(Math.random() * array.length);
+  return (array.slice(rando).join(".") + array.slice(0, rando).join("."))
+}
 
-var content3 = "Shoreditch flannel small batch polaroid Bushwick, Kickstarter keffiyeh Schlitz PBR master cleanse. Gentrify organic slow-carb blog, iPhone yr PBR&B twee kogi Pitchfork selvage put a bird on it. Raw denim freegan tilde butcher, you probably haven't heard of them Etsy ugh normcore keytar fashion axe selvage heirloom lomo Helvetica Pinterest. Organic sartorial mustache Carles Pinterest American Apparel, shabby chic cold-pressed. Asymmetrical dreamcatcher deep v raw denim, Shoreditch wolf messenger bag Truffaut. Messenger bag food truck occupy, salvia kitsch tote bag paleo art party meditation crucifix XOXO PBR asymmetrical. Mustache Neutra squid, dreamcatcher fashion axe cray chillwave literally heirloom put a bird on it.".replace(/'/g, "")
+function markdownify(raw) {
+  var array = raw.split(".");
+  array.forEach(function(sentence, index){
+    var zeroToFour = Math.floor(Math.random() * 5);
+    switch (zeroToFour) {
+      case 0:
+        array[index] = "\n###" + sentence + ".\n\n";
+        break;
+      case 1:
+        array[index] = "\n*" + sentence.split(",").join(".\n*") + ".\n\n";
+        break;
+      default:
+        array[index] = sentence + ".";
+        break;
+    }
+  })
+  return array.join("");
+}
 
 
 db. serialize(function(){
@@ -13,9 +34,9 @@ db. serialize(function(){
   db.run("INSERT INTO users (name, email, team) VALUES ('henrietta', 'henrietta@henrietta.uh', 'executicution');");
   db.run("INSERT INTO users (name, email, team) VALUES ('adelaide', 'adelaide@henrietta.uh', 'polydenigration');");
   db.run("INSERT INTO users (name, email, team) VALUES ('todd', 'todd@henrietta.uh', 'blood snorkling');");
-  db.run("INSERT INTO articles (creation_date, subject, category, content, user_id) VALUES (" + Date.parse('02/06/2666') + ",'accelerating resource exhaustion', 'blue-sky thinking', '" + content1 +"', 3);")
-  db.run("INSERT INTO articles (creation_date, subject, category, content, user_id) VALUES (" + Date.parse('08/08/2019') + ",'maximizing prosumer battle-royale', 'issues', '" + content2 +"', 2);")
-  db.run("INSERT INTO articles (creation_date, subject, category, content, user_id) VALUES (" + Date.parse('02/06/2666') + ",'make time for nanny interface', 'work-life balance', '" + content3 +"', 3);");
+  db.run("INSERT INTO articles (creation_date, subject, category, content, user_id) VALUES (" + Date.parse('02/06/2666') + ",'accelerating resource exhaustion', 'blue-sky thinking', '" + markdownify(randStart(ipsumJunk)) +"', 3);")
+  db.run("INSERT INTO articles (creation_date, subject, category, content, user_id) VALUES (" + Date.parse('08/08/2019') + ",'maximizing prosumer battle-royale', 'issues', '" + markdownify(randStart(ipsumJunk)) +"', 2);")
+  db.run("INSERT INTO articles (creation_date, subject, category, content, user_id) VALUES (" + Date.parse('02/06/2666') + ",'make time for nanny interface', 'work-life balance', '" + markdownify(randStart(ipsumJunk)) +"', 3);");
   db.run("INSERT INTO edits (edit_date, article_id, user_id) VALUES (" + Date.parse('01/01/3000') + ", 1, 2);");
   db.run("INSERT INTO edits (edit_date, article_id, user_id) VALUES (" + Date.parse('01/03/3000') + ", 1, 2);");
   db.run("INSERT INTO edits (edit_date, article_id, user_id) VALUES (" + Date.parse('02/14/3010') + ", 3, 1);");
